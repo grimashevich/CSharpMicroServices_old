0ab5613b94e8319bab53bab99194f89655b9fc31
Konstantin Grimashevich <k.grimashevich@gmail.com>
2022-04-28 13:12:42 +0300

    Lesson 1

---
+++ Lesson1/Controllers/CrudController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Lesson1.Controllers
+{
+    [Route("api/curd")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        private WeatherHolder _holder;
+
+        public CrudController(WeatherHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("add-weather")]
+        public IActionResult Create([FromQuery] int temp, [FromQuery] string date)
+        {
+            _holder.AddWeather(new Weather(temp, date));
+            return Ok();
+        }
+        
+        [HttpGet("get-weather")]
+        public IActionResult Read(string stardDate, [FromQuery] string endDate)
+        {
+            return Ok(_holder.GetWeather(stardDate, endDate));
+        }
+
+
+        [HttpPatch("update-weather")]
+        public IActionResult Update([FromQuery] int temp, [FromQuery] string date)
+        {
+            return Ok(_holder.UpdateWeather(date, temp));
+        }
+
+        [HttpDelete("delete-weather")]
+        public IActionResult Delete([FromQuery] string stardDate, [FromQuery] string endDate)
+        {
+            _holder.DeleteWeather(stardDate, endDate);
+            return Ok();
+        }
+    } 
+}
---
+++ Lesson1/WeatherHolder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson1
+{
+    public class WeatherHolder
+    {
+        private List<Weather> _weatherList;
+
+        public void AddWeather(Weather newWeather) => _weatherList.Add(newWeather);
+
+        public WeatherHolder()
+        {
+            _weatherList = new List<Weather>();
+        }
+
+
+        public List<Weather> GetWeather(string startDate, string EndDate)
+        {
+            DateTime start, end;
+            ParseDate(startDate, EndDate, out start, out end);
+            List<Weather> result = _weatherList.Where(x => x.Date >= start && x.Date <= end).ToList();
+            return result;
+        }
+
+        public List<Weather> GetWeather() => GetWeather("", "");
+
+        public bool UpdateWeather(string date, int temp)
+        {
+            DateTime curDate;
+            if (!DateTime.TryParse(date, out curDate))
+                return false;
+            foreach (Weather weather in _weatherList)
+            {
+                if (weather.Date == curDate)
+                {
+                    weather.TemperatureC = temp;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void DeleteWeather(string startDate, string EndDate)
+        {
+            DateTime start, end;
+            if (!DateTime.TryParse(startDate, out start) || !DateTime.TryParse(EndDate, out end))
+                return;
+            List<Weather> toDelete = _weatherList.Where(x => x.Date >= start && x.Date <= end).ToList();
+            foreach (Weather weather in toDelete)
+            {
+                if (weather.Date >= start && weather.Date <= end)
+                    _weatherList.Remove(weather);
+            }
+        }
+
+        private void ParseDate(string startDate, string EndDate, out DateTime start, out DateTime end)
+        {
+            if (!DateTime.TryParse(startDate, out start))
+                start = DateTime.MinValue;
+            if (!DateTime.TryParse(EndDate, out end))
+                end = DateTime.MaxValue;
+        }
+    }
+}
