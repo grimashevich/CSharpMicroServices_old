3a107273f4316b1d2d39901de2e709213a8799ac
Konstantin Grimashevich <k.grimashevich@gmail.com>
2022-05-11 15:16:35 +0300

    Leeson 2

---
+++ Lesson2/Metrics/MetricsAgentTests/DotNetErrorsCountControllerrTests.cs
@@ -0,0 +1,31 @@
+﻿using MetricsAgent.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MetricsAgentTests
+{
+	public class DotNetErrorsCountControllerrTests
+	{
+		DotNetErrorsCountControllerr _dotNetErrorsCountControllerr;
+
+		public DotNetErrorsCountControllerrTests()
+		{
+			_dotNetErrorsCountControllerr = new DotNetErrorsCountControllerr();
+		}
+
+		[Fact]
+		public void ReturnTypeTest()
+		{
+			TimeSpan t1 = TimeSpan.FromSeconds(1);
+			TimeSpan t2 = TimeSpan.FromSeconds(2);
+
+			IActionResult result = _dotNetErrorsCountControllerr.GetMetrics(t1, t2);
+			Assert.IsAssignableFrom<IActionResult>(result);
+		}
+	}
+}
---
+++ Lesson2/Metrics/MetricsAgentTests/RAMControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using MetricsAgent.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MetricsAgentTests
+{
+	public class RAMControllerTests
+	{
+		RAMController _ramController;
+
+		public RAMControllerTests()
+		{
+			_ramController = new RAMController();
+		}
+
+		[Fact]
+		public void ReturnTypeTest()
+		{
+			TimeSpan t1 = TimeSpan.FromSeconds(1);
+			TimeSpan t2 = TimeSpan.FromSeconds(2);
+
+			IActionResult result = _ramController.GetMetrics(t1, t2);
+			Assert.IsAssignableFrom<IActionResult>(result);
+		}
+	}
+}
